c1a297bc8394458e39d9e876c7ec4ca733d3f9c1
zlj <zlj@A14S-ZLJ>
2020-11-28 13:06:03 +0800

    first

---
+++ ZLOG/main.cpp
@@ -0,0 +1,9 @@
+#include"zlog.h"
+
+int main(void)
+{
+	Zlog zlog;
+
+
+	return 0;
+}
---
+++ ZLOG/zlog.h
@@ -0,0 +1,17 @@
+#pragma once
+/*这个类用来保存日志到文件中，自己用*/
+
+
+class Zlog
+{
+public:
+	Zlog();
+
+private:
+
+};
+
+Zlog::Zlog()
+{
+}
+
